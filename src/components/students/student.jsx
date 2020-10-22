06033f7af3fb94710c312030263b0ec2314833c8
Julie Tran <kimtran767@gmail.com>
2020-10-21 22:20:14 -0700

    got average grades

--- src/components/students/student.jsx
+++ src/components/students/student.jsx
@@ -4,12 +4,22 @@ const Student = ({student}) => {
 
     const { firstName, email, company, skill, grades, pic } = student;
 
-
+    const avgGrades = () => {
+        let sum = 0;
+        grades.map(el => sum += parseInt(el));
+        return sum / grades.length;
+    }
+    
     return (
         <div className='student'>
             <img src={pic} alt=""/>
             <div className='student-info'>
                 <h2>{firstName}</h2>
+                <div>{email}</div>
+                <div>{company}</div>
+                <div>{skill}</div>
+                <div>{grades[0]}</div>
+    <div>{avgGrades()}</div>
             </div>
         </div>
     )
