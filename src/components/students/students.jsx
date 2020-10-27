e57010b2875caa8fa689e2b916cc062ef81c700b
Julie Tran <kimtran767@gmail.com>
2020-10-27 13:42:25 -0700

    updated search on change method working, tested with consolelog

--- src/components/students/students.jsx
+++ src/components/students/students.jsx
@@ -9,14 +9,27 @@ const Students = () => {
     const dispatch = useDispatch();
     const students = useSelector(state => Object.values(state.students));
 
-    const [displayGrades, setDisplayGrades] = useState(false);
+    const [ displayGrades, setDisplayGrades ] = useState(false);
+    const [ searchStudent, setSearchStudent ] = useState('');
 
   useEffect(() => {
     dispatch(fetchStudents());
   }, []);
 
+  const updateSearch = (e) => {
+    setSearchStudent(e.target.value.substr(0,20));
+  }
+
   return (
     <div className='students'>
+        <div className='name-searchbar'>
+          <input 
+            onChange={updateSearch}
+            type="text" 
+            id='name-input' 
+            placeholder='Search by name'/>
+            {console.log(searchStudent)}
+        </div>
         <ul>
           {students.map((student, i) => (
               <li key={i}>
